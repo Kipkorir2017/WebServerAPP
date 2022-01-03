4f455f22c7a177db7f2bbc25883cdd88993376ff
Kipkorir2017 <pkobenj41@gmail.com>
2022-01-03 11:46:16 +0300

    add a server function to listen to the port

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ const requestListener=function(req,res){
 	res.writeHead(200);
 	res.end("Welcome TO web server");
 };
+
+const server =http.createServer(requestListener);
+server.listen(port,host,()=>{
+	console.log(`Server is running on http://${host}:${port}`)
+;})
