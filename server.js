efbf58ffd18292fd78c5637c604c825aa998fc9c
Kipkorir2017 <pkobenj41@gmail.com>
2022-01-03 11:38:02 +0300

    add requestListener function

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const http=require("http");
 
 const host = 'localhost';
 const port = 8000;
+
+
+// request listener function
+const requestListener=function(req,res){
+	res.writeHead(200);
+	res.end("Welcome TO web server");
+};
